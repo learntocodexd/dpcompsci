bc246562664f8ab11865c42d1e6ad8be1d04a806
learntocodexd <77285320+learntocodexd@users.noreply.github.com>
2021-03-03 19:04:54 -0800

    Update caesarcipher.py

--- caesarcipher.py
+++ caesarcipher.py
@@ -1,12 +1,7 @@
 def caesar(plainText, shift): 
     cipherText = ""
     for ch in plainText:
-        if ch.isalpha():
-            stayInAlphabet = ord(ch) + shift 
-            if stayInAlphabet > ord('z'):
-                stayInAlphabet -= 26
-        else:
-            stayInAlphabet = ord(ch) + shift
+        stayInAlphabet = ord(ch) + shift 
         finalLetter = chr(stayInAlphabet)
         cipherText += finalLetter
     print("Your ciphertext is: ", cipherText)
