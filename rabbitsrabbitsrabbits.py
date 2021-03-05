14e3d272c024d71192788926a3a13180390e3719
learntocodexd <77285320+learntocodexd@users.noreply.github.com>
2021-03-05 02:37:07 -0800

    Create rabbitsrabbitsrabbits.py

---
+++ rabbitsrabbitsrabbits.py
@@ -0,0 +1,25 @@
+"""
+python3
+print rabbit growth rate in pairs as they grow from babies to adults to a csv file
+"""
+
+
+def rabbits(startAdultRabbits, startBabyRabbits, monthlyReproductionRate, totalCages):
+
+    adultRabbits = startAdultRabbits
+    babyRabbits = startBabyRabbits
+    rabbitCages = totalCages
+    reproductionRate = monthlyReproductionRate
+    totalRabbits = adultRabbits + babyRabbits
+
+    print(babyRabbits, adultRabbits, totalRabbits)
+
+    while totalRabbits < rabbitCages:
+        tempBabyRabbits = babyRabbits
+        adultRabbits += tempBabyRabbits
+        babyRabbits += adultRabbits
+        totalRabbits = adultRabbits + babyRabbits
+        print(babyRabbits, adultRabbits, totalRabbits)
+
+
+rabbits(1, 0, 1, 500)
