0d40d313049d4417afd4aa72c5fadc66059d8f7f
learntocodexd <77285320+learntocodexd@users.noreply.github.com>
2021-03-06 23:57:46 -0800

    Create rabbitscsv.py

---
+++ rabbitscsv.py
@@ -0,0 +1,34 @@
+"""
+python3
+print rabbit growth rate in pairs as they grow from babies to adults to a csv file
+"""
+
+
+import csv
+
+
+with open("rabbits.csv", "w", newline="") as f:
+    thewriter = csv.writer(f)
+
+    adultRabbits = 1
+    babyRabbits = 0
+    rabbitCages = 500
+    reproductionRate = 1
+    tempBabyRabbits = 0
+    month = 1
+    totalRabbits = adultRabbits + babyRabbits
+
+    thewriter.writerow([month, adultRabbits, babyRabbits, totalRabbits])
+
+    while totalRabbits < rabbitCages:
+        tempBabyRabbits -= tempBabyRabbits #clear temp baby rabbits
+        tempBabyRabbits = adultRabbits * reproductionRate #adults making babies
+        adultRabbits += babyRabbits #babies mature into adults
+        babyRabbits = tempBabyRabbits #temp babies to real babies
+        totalRabbits = adultRabbits + babyRabbits #creating total
+        month += 1
+        thewriter.writerow([month, adultRabbits, babyRabbits, totalRabbits])
+
+    f.close()
+
+print("csv file created")
