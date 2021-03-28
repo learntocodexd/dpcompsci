c12133d7f5fbe9db9aa63c0a2b2bf37d0035891b
learntocodexd <77285320+learntocodexd@users.noreply.github.com>
2021-03-28 13:42:06 -0700

    Update stocks_data_script.py

--- stocks_data_script.py
+++ stocks_data_script.py
@@ -10,38 +10,51 @@ when lowest find date
 finally compare all three max/min dicts
 get high/low of all price and the date
 write to console and text file(append) all these data points
-
 '''
 
 
 import csv
 
 
+stocks = []
+aapl_stocks = []
+ibm_stocks = []
+msft_stocks = []
+unknown_stocks = []
+
 #opens the csv file
 with open('stocks_data.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+    csv_reader = csv.DictReader(csv_file, delimiter=',')
     
-    stocks = {'Symbol': [], 'Date': [], 'Price': []}
-    aapl_stocks = {'Symbol': [], 'Date': [], 'Price': []}
-    ibm_stocks = {'Symbol': [], 'Date': [], 'Price': []}
-    msft_stocks = {'Symbol': [], 'Date': [], 'Price': []}
-
-    #next skips the headers of the csv, that is why line count starts at 1
-    next(csv_reader)
-
-    #sort the csv file into 3 dicts according to the stock
+    #sort the csv file into 4 lists of dicts according to the stock
     for row in csv_reader:
-        if row[0] == "AAPL":
-            aapl_stocks['Date'].append(row[1])
-            aapl_stocks['Price'].append(float(row[2]))
+        if row['Symbol'] == "AAPL":
+            aapl_stocks.append({
+                'Symbol': row['Symbol'],
+                'Date': row['Date'],
+                'Price': row['Adj Close']
+            })
 
-        elif row[0] == "IBM":
-            ibm_stocks['Date'].append(row[1])
-            ibm_stocks['Price'].append(float(row[2]))
+        elif row['Symbol'] == "IBM":
+            ibm_stocks.append({
+                'Symbol': row['Symbol'],
+                'Date': row['Date'],
+                'Price': row['Adj Close']
+            })
 
-        elif row[0] == "MSFT":
-            msft_stocks['Date'].append(row[1])
-            msft_stocks['Price'].append(float(row[2]))
+        elif row['Symbol'] == "MSFT":
+            msft_stocks.append({
+                'Symbol': row['Symbol'],
+                'Date': row['Date'],
+                'Price': row['Adj Close']
+            })
+
+        else:
+            unknown_stocks.append({
+                'Symbol': row['Symbol'],
+                'Date': row['Date'],
+                'Price': row['Adj Close']
+            })
 
 #finds the high by iterating through dict and comparing each value
 #also creates a count of how many of each script
@@ -49,84 +62,107 @@ aapl_stock_high = 0
 aapl_stock_total = 0
 aapl_stock_total_count = 0
 aapl_stock_low = 1000
-for value in aapl_stocks["Price"]:
-    if value > aapl_stock_high:
-        aapl_stock_high = value
-        aapl_stock_total += value
-        aapl_stock_total_count += 1
+for dic in aapl_stocks:
+    if float(dic['Price']) > aapl_stock_high:
+        aapl_stock_high = float(dic['Price'])
+        aapl_stock_total += float(dic['Price'])
+        aapl_stock_total += 1
+        aapl_stock_high_dict = dic
+
     else:
-        if value < aapl_stock_low:
-            aapl_stock_low = value
-            aapl_stock_total += value
+        if float(dic['Price']) < aapl_stock_low:
+            aapl_stock_low = float(dic['Price'])
+            aapl_stock_total += float(dic['Price'])
             aapl_stock_total_count += 1
+            aapl_stock_low_dict = dic
+            
         else:
-            aapl_stock_total += value
+            aapl_stock_total += float(dic['Price'])
             aapl_stock_total_count += 1
 
-#prints stock high, computes stock average and prints\
-print(f"\nAAPL \n---")
-print(f"Max: {aapl_stock_high}.")
-print(f"Min: {aapl_stock_low}.")
 aapl_stock_average = aapl_stock_total / aapl_stock_total_count
-print(f"Average: {aapl_stock_average}.")
+aapl_stock_high_date = aapl_stock_high_dict["Date"]
+aapl_stock_low_date = aapl_stock_low_dict["Date"]
 
 #repeats for ibm
 ibm_stock_high = 0
 ibm_stock_total = 0
 ibm_stock_total_count = 0
 ibm_stock_low = 1000
-for value in ibm_stocks["Price"]:
-    if value > ibm_stock_high:
-        ibm_stock_high = value
-        ibm_stock_total += value
-        ibm_stock_total_count += 1
+for dic in ibm_stocks:
+    if float(dic['Price']) > ibm_stock_high:
+        ibm_stock_high = float(dic['Price'])
+        ibm_stock_total += float(dic['Price'])
+        ibm_stock_total += 1
+        ibm_stock_high_dict = dic
+
     else:
-        if value < ibm_stock_low:
-            ibm_stock_low = value
-            ibm_stock_total += value
+        if float(dic['Price']) < ibm_stock_low:
+            ibm_stock_low = float(dic['Price'])
+            ibm_stock_total += float(dic['Price'])
             ibm_stock_total_count += 1
+            ibm_stock_low_dict = dic
+            
         else:
-            ibm_stock_total += value
+            ibm_stock_total += float(dic['Price'])
             ibm_stock_total_count += 1
 
-print(f"\nIBM \n---")
-print(f"Max: {ibm_stock_high}.")
-print(f"Min: {ibm_stock_low}.")
 ibm_stock_average = ibm_stock_total / ibm_stock_total_count
-print(f"Average: {ibm_stock_average}.")
+ibm_stock_high_date = ibm_stock_high_dict["Date"]
+ibm_stock_low_date = ibm_stock_low_dict["Date"]
 
 #repeats for msft
 msft_stock_high = 0
 msft_stock_total = 0
 msft_stock_total_count = 0
 msft_stock_low = 1000
-for value in msft_stocks["Price"]:
-    if value > msft_stock_high:
-        msft_stock_high = value
-        msft_stock_total += value
-        msft_stock_total_count += 1
+for dic in msft_stocks:
+    if float(dic['Price']) > msft_stock_high:
+        msft_stock_high = float(dic['Price'])
+        msft_stock_total += float(dic['Price'])
+        msft_stock_total += 1
+        msft_stock_high_dict = dic
+
     else:
-        if value < msft_stock_low:
-            msft_stock_low = value
-            msft_stock_total += value
+        if float(dic['Price']) < msft_stock_low:
+            msft_stock_low = float(dic['Price'])
+            msft_stock_total += float(dic['Price'])
             msft_stock_total_count += 1
-        else:
-            msft_stock_total += value
-            msft_stock_total_count += 1
+            msft_stock_low_dict = dic
             
-print(f"\nMSFT \n---")
-print(f"Max: {msft_stock_high}.")#place date here
-print(f"Min: {msft_stock_low}.")#place date here
+        else:
+            msft_stock_total += float(dic['Price'])
+            msft_stock_total_count += 1
+
 msft_stock_average = msft_stock_total / msft_stock_total_count
-print(f"Average: {msft_stock_average}.")#place date here
+msft_stock_high_date = msft_stock_high_dict["Date"]
+msft_stock_low_date = msft_stock_low_dict["Date"]
 
 
+print(f"\nAAPL \n---")
+print(f"Max: {aapl_stock_high} on {aapl_stock_high_date}.")
+print(f"Min: {aapl_stock_low} on {aapl_stock_low_date}.")
+print(f"Average: {aapl_stock_average}.")
 
-#redundant print of total price
+print(f"\nIBM \n---")
+print(f"Max: {ibm_stock_high} on {ibm_stock_high_date}.")
+print(f"Min: {ibm_stock_low} on {ibm_stock_low_date}.")
+print(f"Average: {ibm_stock_average}.")
 
-#ignore this something we might need if we have to fundamentally change the whole script
-'''
-        stocks['symbol'].append(row[0])
-        stocks['date'].append(row[1])
-        stocks['price'].append(float(row[2]))
-'''
+print(f"\nMSFT \n---")
+print(f"Max: {msft_stock_high} on {msft_stock_high_date}.")
+print(f"Min: {msft_stock_low} on {msft_stock_low_date}.")
+print(f"Average: {msft_stock_average}.")
+
+
+all_stock_max = (max(
+    float(aapl_stock_high_dict['Price']), 
+    float(ibm_stock_high_dict['Price']), 
+    float(msft_stock_high_dict['Price'])
+))
+
+all_stock_low = (min(
+    float(aapl_stock_low_dict['Price']), 
+    float(ibm_stock_low_dict['Price']), 
+    float(msft_stock_low_dict['Price'])
+))
