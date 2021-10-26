25e6af27b633423c4cfd870249498a08b8b78a00
dirtydp <80020567+dirtydp@users.noreply.github.com>
2021-10-26 12:31:38 -0600

    Add files via upload

---
+++ sololearn_2.py
@@ -0,0 +1,29 @@
+'''
+print("Python is fun!")
+print('Always look on the bright side of life')
+#the symbols (' or ") are called delimiters
+print('Brians\'s mother: He\'s not an angel. He\'s a very naughty boy!')
+# more explanation needed
+print('I\'m learning!')
+'''
+'''
+print('One\nTwo\nThree')
+#\n used to create new lines in output
+print('one\tTwo\tThree')
+#\t represents a new tab
+print('Hello \nworld')
+print("""this
+is a
+multiline
+text""")
+print("""Thisisgreat""")
+'''
+
+print("Spam" + 'eggs')
+#this is called concatenation
+print("2" + "2")
+print("56" + "balls")
+print("spam" * 3)
+print(4 * '2')
+#doesnt matter which comes first but usually string is first
+print("spam" * 4.0)
