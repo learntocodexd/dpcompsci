a63ae91691ac2909e7a8e79b788e5515b7ebe7f1
dirtydp <80020567+dirtydp@users.noreply.github.com>
2021-10-26 14:29:06 -0600

    Add files via upload

---
+++ sololearn_3_strings_variables_1.py
@@ -0,0 +1,59 @@
+'''
+user  = "James"
+age  = 42
+
+
+x= 7
+print(x)
+print(x + 3)
+print(x)
+
+
+spam = "eggs"
+print(spam * 3)
+'''
+'''
+x = 123.456
+print(x)
+x = "This is a string"
+print(x + "!")
+'''
+'''
+x = "howdy"
+print(x + " stinking dog, you made me look bad!")
+'''
+'''
+x = input()
+print(x)
+'''
+'''
+name = input("Enter your name: ")
+print("Hello, " + name)
+'''
+'''
+age = int(input())
+print(age)
+
+age = 42
+print("His age is " + str(age))
+
+age = 6.2
+print("His age is " + str(age))
+'''
+'''
+#the code below takes numbers as strings, turns them back into numbers and adds them together to make 6
+x = "2"
+y = "4"
+z = int(x) + int(y)
+print(z)
+'''
+'''
+name  = input("Enter your name here: ")
+age = input("Enter your age here: ")
+
+print(name + " is " + age)
+
+x = int(input())
+y = int(input())
+print(x + y)
+'''
