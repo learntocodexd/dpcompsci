19deeeced2e3615cd19ad41fc50f308f6f389de9
dirtydp <80020567+dirtydp@users.noreply.github.com>
2021-10-26 12:17:12 -0600

    Add files via upload

---
+++ sololearn_1.py
@@ -0,0 +1,52 @@
+'''
+print('Hello world!')
+print('Hello world!')
+print('Spam and eggs...')
+'''
+'''
+print(2+2)
+print(5 + 4 - 3)
+print(1 + 2 + 3)
+'''
+'''
+print(2 * (3 + 4))
+print(10 / 2)
+print((4 + 8) / 2)
+'''
+'''
+print(3 / 4)
+print(0.42)
+print(1/3)
+'''
+'''
+print(8 / 2)
+print(6 * 7.0)
+print(4 + 1.65)
+#float can be added to an integer. python converts it to a float
+print(1 + 2 + 3 + 4.0 + 5)
+'''
+'''
+print(2**5)
+print(5**3)
+print( 9 ** (1/2))
+#9 raised to the 1/2 power results in the square root of 9
+print(8**(1/3))
+'''
+'''
+print( 20 // 6 )
+#this is floor division. used to determine how many times 6 can go into 20 which is 3
+print( 10 // 4 )
+# the % is modula operator and is used to get the remainder of a division
+print( 20 % 6)
+# floor division and modula do the opposite thing. for 6 into 20, floor gives 3 and modula gives remainder of 2
+print( 5 % 4 )
+print( 1.25 % 0.5 )
+print( 7 % ( 5 // 2))
+#floor division will come out as 2 with remainder 1. then the modular will take the answer '2' and see what remains when 7 is divide by it = 1
+print( 5 % 5 )
+'''
+
+#QUIZ
+print( 1 + 4 * 3 )
+print(( 3 ** 2 ) // 2 )
+print( 100 // 2 )
