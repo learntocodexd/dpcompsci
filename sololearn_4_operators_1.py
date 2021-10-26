cd1d0edc5fd42085d02f101fb93ed9a59b37e692
dirtydp <80020567+dirtydp@users.noreply.github.com>
2021-10-26 14:54:25 -0600

    Add files via upload

---
+++ sololearn_4_operators_1.py
@@ -0,0 +1,65 @@
+'''
+x= 2
+print(x)
+
+x += 3
+print(x)
+
+x= 4
+x *= 3
+print(x)
+'''
+'''
+x = "spam"
+print(x)
+
+x +="eggs"
+print(x)
+
+x = "apple"
+x += "tree"
+print(x)
+
+x = "a"
+x *= 3
+print(x)
+'''
+'''
+num = int(input())
+print(num)'''
+
+#print(num:=int(input()))
+#need assistance. walrus operator does not work
+#walrus takes text as input, assigns it to the name variable and outputs it
+
+#QUIZ
+'''
+spam = "7"
+spam = spam +"0"
+eggs = int(spam) + 3
+print(float(eggs))
+
+#age = int(input())
+#print(age + 8)
+
+x = 5
+y = x + 3
+y = int(str(y) + "2")
+print(y)
+
+x = 3
+num = 17
+print(num % x)
+
+name = input()
+print("Welcome home, " + name)
+
+#write a code to take two integers as input and output their sum
+#sample input: 2, 8
+#sample output: 10
+
+x = int(input())
+y = int(input())
+print( x + y )
+'''
+
